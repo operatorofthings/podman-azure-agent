c55f3f8094bdde9e624130bef5fe83ebb52f807e
Johannes Steiner <johannes.steiner@protonmail.com>
2023-05-03 22:04:49 +0200

    changed --privileged to whats really needed by podman

--- runAgent.sh
+++ runAgent.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 # Script to run image (param 1 or 'podman-agent') with tag (param 2 or 'latest')
 # Runs in detached mode (-d)
-# Runs privileged
+# Runs (softer) privileged mode with --cap-add SYS_ADMIN --device /dev/fuse
+# -> SYS_ADMIN is needed for mount/umount functionality
+# -> /dev/fuse exposes the FUSE device to the container
 # Takes $ENV_VARS from .envrc
 # Is named 'podman-agent'
 
 IMAGE_NAME="${1:-podman-agent}"
 IMAGE_TAG="${2:-latest}"
 
-podman run -d --name=podman-agent --privileged --rm -e "AZP_POOL=${AZP_POOL:-Default}" -e "AZP_URL=$AZP_URL" -e "AZP_TOKEN=$AZP_TOKEN" -e "AZP_AGENT_NAME=$AZP_AGENT_NAME" $IMAGE_NAME:$IMAGE_TAG
+podman run -d --name=podman-agent --cap-add SYS_ADMIN --device /dev/fuse --rm -e "AZP_POOL=${AZP_POOL:-Default}" -e "AZP_URL=$AZP_URL" -e "AZP_TOKEN=$AZP_TOKEN" -e "AZP_AGENT_NAME=$AZP_AGENT_NAME" $IMAGE_NAME:$IMAGE_TAG
